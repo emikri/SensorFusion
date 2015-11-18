1191da739e80517ac999ba1de5213621e08e94db
NastyFever <k-lime@hotmail.com>
2015-11-18 11:37:58 +0100

    Add pause at end to prevent the prompt to close to fast

--- SensorFusion/SensorFusion/Main.cpp
+++ SensorFusion/SensorFusion/Main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 
 int main(char argv, char *args[]) {
+	
+	
+	
+	/*
+	 * For pausing at the end.	
+	*/ 
+	string pause;
+	getline(cin, pause);
 
-	printf("hej\n");
 	return 0;
 }
