1d2330c5ff00dd1e976039a1815054ebab7ada10
zpettzon <joperss@student.chalmers.se>
2015-12-04 17:20:50 +0100

    Correct files

--- Sensor Fusion/broadcastreceiver/kalman_object.cpp
+++ Sensor Fusion/broadcastreceiver/kalman_object.cpp
@@ -1,7 +1,26 @@
 #include "kalman_object.h"
+#include "kalman.h"
+
 
 kalman_object::kalman_object()
 {
+    orientation = QQuaternion(1.0f, 0.0f, 0.0f, 0.0f);
+    ex = 0;
+    ey = 0;
+    ez = 0;
+}
 
+void kalman_object::updateOrientation(float gx, float gy, float gz, float ax, float ay, float az)
+{
+    //TODO: fix units
+    ex = Kalman::getAngle(ax, gx, 0.001f);
+    ey = Kalman::getAngle(ay, gy, 0.001f);
+    ez = Kalman::getAngle(az, gz, 0.001f);
+    orientation = QQuaternion::fromEulerAngles(ex, ey, ez);
+}
+
+QQuaternion kalman_object::getRotation()
+{
+    return orientation;
 }
 
--- Sensor Fusion/broadcastreceiver/kalman_object.h
+++ Sensor Fusion/broadcastreceiver/kalman_object.h
@@ -1,11 +1,19 @@
 #ifndef KALMAN_OBJECT_H
 #define KALMAN_OBJECT_H
 
+#include <QQuaternion>
+
 
 class kalman_object
 {
 public:
     kalman_object();
+    void updateOrientation(float, float, float, float, float, float);
+    QQuaternion getRotation();
+
+private:
+    QQuaternion orientation;
+    float ex, ey, ez;
 };
 
 #endif // KALMAN_OBJECT_H
