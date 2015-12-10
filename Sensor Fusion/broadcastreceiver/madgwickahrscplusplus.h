b446e4d70f417ed420a47c93a5ef30d8efa13b31
Emil Kristiansson <k-lime@hotmail.com>
2015-12-10 13:09:05 +0100

    Remove commented old code

--- Sensor Fusion/broadcastreceiver/madgwickahrscplusplus.h
+++ Sensor Fusion/broadcastreceiver/madgwickahrscplusplus.h
@@ -11,7 +11,6 @@ public:
 
     float beta;
     float q0, q1, q2, q3;
-//    void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
     void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
     QList<float> getOrientation();
     void updateOrientation(float, float, float,float, float, float,float, float, float);
